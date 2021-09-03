6432c7c04ef56cfdbae82a5ec3f630cdf02b4079
AstroKabutar <77823679+AstroKabutar@users.noreply.github.com>
2021-09-03 16:06:25 +0000

    Update bitset.hpp

--- bitset_header/bitset.hpp
+++ bitset_header/bitset.hpp
@@ -56,6 +56,23 @@ public:
 	
 //	#include "bitset.inl"
 };
-#include "bitset.inl"
+//#include "bitset.inl"
+template<std::size_t size>
+ptd::bitset<size>::bitset(std::size_t storage) {	m_storage = storage;	}
+
+template<std::size_t size>
+bool ptd::bitset<size>::ison(int index) const {	return (1<<index) & m_storage; }
+
+template<std::size_t size>
+ptd::bitset<size>& ptd::bitset<size>::flip(int index) {	m_storage ^= (1<<index); return *this;	}
+
+template<std::size_t size>
+ptd::bitset<size>& ptd::bitset<size>::invert() {	m_storage = ~m_storage; return *this;	}
+
+template<std::size_t size>
+ptd::bitset<size>& ptd::bitset<size>::on(int index) {	m_storage |= (1<<index); return *this;	}
+
+template<std::size_t size>
+ptd::bitset<size>& ptd::bitset<size>::off(int index){	m_storage &= ~(1<<index); return *this;	}
 
 #endif
